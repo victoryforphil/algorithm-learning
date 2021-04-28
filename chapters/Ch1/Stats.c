8f9cdfb1aa0a5eca3db6c22e7a01f25447137734
Alex Carter <victoryforphil@gmail.com>
2021-04-28 12:54:23 -0700

    Stats Finished

--- chapters/Ch1/Stats.c
+++ chapters/Ch1/Stats.c
@@ -2,18 +2,54 @@
 #include <stdio.h>
 #include "Node.h"
 #include "Bag.h"
+#include <math.h>
+#include<string.h>
 int main(){
     printf("Stats App\n");
 
     ALGBag* bag = ALGBag_Create();
     
-    for(size_t i=0;i<100; i++){
-        ALGBag_Add(bag, &i);
+    float myInt;
+    int result = scanf("%f", &myInt);
+    ALGBag_Add(bag, &myInt);
+    float toStore = 5;
+    while (toStore > 1) {
+        
+        
+        float newInt;
+        ALGBag_Add(bag, &newInt);
+        result = scanf("%f", &newInt);
+        toStore--;
     }
 
     printf("ALG Bag Size: %d\n",ALGBag_GetLength(bag));
 
+    int n = ALGBag_GetLength(bag);
+    float sum = 0.0;
 
+    ALGNode* node = bag->first;
+    while(node != NULL){
+        printf("item %f\n",  *(float *)node->item);
+        sum += *(float *)node->item;
+        node = node->next;
+    }
+    
+
+    float mean = sum/n;
+
+    node = bag->first;
+    while(node != NULL){
+        float x = *(float *)node->item;
+        sum += (x - mean) * (x - mean);
+        node = node->next;
+    }
+    float stdev = sqrt(sum/(n-1));
+
+
+    printf("Mean %f\n", mean);
+    printf("stdev %f\n", stdev);
+
+    ALGNode_Destroy(node);
     ALGBag_Destroy(bag);
     
 }
